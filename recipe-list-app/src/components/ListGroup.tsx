8a289a721c8409de6902fed53d23c5d03f1b4a9c
Tobias Lindberg <uet.lindberg@outlook.com>
2024-05-06 14:06:58 +0200

    Rendering static list

--- recipe-list-app/src/components/ListGroup.tsx
+++ recipe-list-app/src/components/ListGroup.tsx
@@ -1,12 +1,21 @@
 function ListGroup() {
+  const listItems = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+  ];
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {listItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
